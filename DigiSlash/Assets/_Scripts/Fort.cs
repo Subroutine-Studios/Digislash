193d9f70ac7c022ff3cb5fb4f84bcdd8594b0c0f
Aki <57061301+akiescarraga@users.noreply.github.com>
2022-11-25 20:04:56 +0800

    created Fort script

---
+++ DigiSlash/Assets/_Scripts/Fort.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fort : MonoBehaviour
+{
+    [SerializeField]
+    private float _leaks = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

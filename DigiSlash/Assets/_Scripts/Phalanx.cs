83a98ffc2bc32dc35c90963488e3272d0671b893
Aki <57061301+akiescarraga@users.noreply.github.com>
2022-11-25 20:02:43 +0800

    added Phalanx enemy

---
+++ DigiSlash/Assets/_Scripts/Phalanx.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Phalynx : MonoBehaviour
+{
+    [SerializeField]
+    private EnemyTracing _enemyTracer;
+
+    [SerializeField]
+    private float _health = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyTracer._AIDestinationTarget.target = GameObject.FindGameObjectWithTag("Gate").GetComponent<Transform>();
+        _enemyTracer._target = GameObject.FindGameObjectWithTag("Gate").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

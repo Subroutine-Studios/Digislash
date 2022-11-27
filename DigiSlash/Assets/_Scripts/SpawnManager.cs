0456276b0644bd81a18ea91a794ddfc11fa6de2e
Aki <57061301+akiescarraga@users.noreply.github.com>
2022-11-27 17:05:12 +0800

    created initial WaveManager

--- DigiSlash/Assets/_Scripts/SpawnManager.cs
+++ DigiSlash/Assets/_Scripts/SpawnManager.cs
@@ -10,10 +10,16 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     private GameObject _enemyContainer;
 
+    private WaveManager _waveManager;
+
     private bool _stopSpawning = false;
 
     // Start is called before the first frame update
     void Start()
+    {
+    }
+
+    public void StartSpawning()
     {
         StartCoroutine(SpawnEnemyRoutine());
     }
@@ -21,20 +27,55 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnEnemyRoutine()
     {
+        int enemiesSpawned = 0;
+
         yield return new WaitForSeconds(3.0f); // wait 3 seconds before spawning
 
-
         // while <???>, keep spawning enemies
-        while (_stopSpawning == false)
+        while (_stopSpawning == false && _waveManager.currentWave < 4)
         {
-            Vector3 posToSpawn = new Vector3(Random.Range(-2f, 2f), -6, 0); // position to spawn (x,y,z)
-            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
-            newEnemy.transform.parent = _enemyContainer.transform;
-            yield return new WaitForSeconds(5.0f);
-        }
+            if (enemiesSpawned != _waveManager.enemiesToSpawn)
+            {
+                switch (_waveManager.currentWave)
+                {
+                    // Wave 1
+                    case 1:
+                        Vector3 posToSpawn = new Vector3(Random.Range(-2f, 2f), -6, 0); // position to spawn (x,y,z)
+                        GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+                        newEnemy.transform.parent = _enemyContainer.transform;
 
-        // will never get here
-    }
+                        _waveManager.enemiesLeft++;
+                        enemiesSpawned++;
+                        break;
+
+                    // Wave 2
+                    case 2:
+                        _waveManager.enemiesLeft++;
+                        enemiesSpawned++;
+                        break;
+
+                    // Wave 3
+                    case 3:
+                        _waveManager.enemiesLeft++;
+                        enemiesSpawned++;
+                        break;
+                }
+            }
+            else
+            {
+                _waveManager.startOfWave = false;
+                enemiesSpawned = 0;
+                _stopSpawning = true;
+            }
+        }
+        
+            yield return new WaitForSeconds(5.0f);
+<<<<<<< Updated upstream
+=======
+
+            // will never get here
+>>>>>>> Stashed changes
+        }
 
 
 

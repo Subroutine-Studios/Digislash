1d97c01fb5e220ee3c3827fb34c73d18aeab3ff1
Aki <57061301+akiescarraga@users.noreply.github.com>
2022-11-28 21:23:14 +0800

    fixed overwritten files

--- DigiSlash/Assets/_Scripts/Fort.cs
+++ DigiSlash/Assets/_Scripts/Fort.cs
@@ -9,26 +9,22 @@ public class Fort : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.tag == "Enemy")
+        if (collision.tag == "Enemy" && collision.GetComponent<EnemyTracing>().attackCooldown >= 5f)
         {
-            StartCoroutine(DamageOverTime());
+            _leaks -= 2f;
+            collision.GetComponent<EnemyTracing>().attackCooldown = 0f;
         }
     }
 
-    IEnumerator DamageOverTime()
-    {        
-        _leaks -= 2f;
-        yield return new WaitForSeconds(10f);
-    }
 }

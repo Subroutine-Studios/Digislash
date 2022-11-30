aad9f32e624d21b4debc20716eb1a61d563c779b
alennacayton <cayton.alenna@gmail.com>
2022-12-01 01:05:22 +0800

    Added story dialogue and transition to level 1

--- DigiSlash/Assets/_Scripts/Story.cs
+++ DigiSlash/Assets/_Scripts/Story.cs
@@ -14,11 +14,42 @@ public class Story : MonoBehaviour
     [SerializeField]
     private DialogueManager _dialogueManager;
 
+    //Dialogue first
+    private bool inDialogue = true;
+    [SerializeField]
+    private GameObject orbImage;
+
+    private bool showWeapon = false;
+
     // Start is called before the first frame update
     void Start()
     {
         //Start talking about the tutorial/story
         StartCoroutine(StartDialogue());
+
+        orbImage.gameObject.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        // if (inDialogue && _dialogueManager.done && showWeapon)
+        if (inDialogue && _dialogueManager.done)
+        {
+            inDialogue = false;
+            _dialogueManager.done = false;
+            SceneManager.LoadScene("Level_One");
+        }
+
+        /*
+        else if(!showWeapon)
+        {
+            inDialogue = true;
+            orbImage.gameObject.SetActive(true);
+            StartCoroutine(StartDialogue());
+        }
+        */
+
     }
 
     private IEnumerator StartDialogue()

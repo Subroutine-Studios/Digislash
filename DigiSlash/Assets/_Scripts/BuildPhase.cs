eae3dd2e25da5c1c1e6c5ab2091c916c2f7976d6
alennacayton <cayton.alenna@gmail.com>
2022-12-05 15:07:29 +0800

    Updated Build Phase

--- DigiSlash/Assets/_Scripts/BuildPhase.cs
+++ DigiSlash/Assets/_Scripts/BuildPhase.cs
@@ -6,12 +6,9 @@ using UnityEngine.UI;
 
 public class BuildPhase : MonoBehaviour
 {
-
-
     //proceed button
     [SerializeField]
     private GameObject _proceedBtn;
-
 
     // bullet type buttons
     [SerializeField]
@@ -29,6 +26,18 @@ public class BuildPhase : MonoBehaviour
     [SerializeField]
     private GameObject _rapidFireBtn;
 
+    [SerializeField]
+    private GameObject _equipTraitBtn;
+
+    [SerializeField]
+    private Text _traitEquippedText;
+
+
+    [SerializeField]
+    private Text _bulletText;
+
+
+
     //switch weap button
     [SerializeField]
     private GameObject _weap1Btn;
@@ -44,16 +53,17 @@ public class BuildPhase : MonoBehaviour
     private Text _fcBaseDmgText;
     [SerializeField]
     private Text _fcRpsText;
-
+    [SerializeField]
+    private Text _fcTraitText;
 
     // trait description content
     [SerializeField]
     private Text _traitDescText;
 
+    private static int _selectedTrait = -1; // 0 = multishot, 1 = warped, 2 = rapidfire
 
     // Store values
     public static Weapon[] _weapons;
-
     public static int _selectedWeap; // 0 is Weap 1 , 1 is Weap 2
 
     [SerializeField]
@@ -61,25 +71,19 @@ public class BuildPhase : MonoBehaviour
     [SerializeField]
     private GameObject _prefabB;
 
-
     // player
     private Player _player;
-
 
     void Start()
     {
         // Weapon 1 is selected by default
         _selectedWeap = 0;
-
         _weapons = new Weapon[2];
 
         _weapons[0] = new Weapon("null", "null", "null", 0, 0, 0, 0, 0, 0, _prefabA, _prefabB);
         _weapons[1] = new Weapon("null", "null", "null", 0, 0, 0, 0, 0, 0, _prefabA, _prefabB);
 
-
         Debug.Log("Buld phase start: " + _weapons[_selectedWeap].bullet + " selected weapon : " + _selectedWeap);
-
-
     }
 
 
@@ -115,12 +119,14 @@ public class BuildPhase : MonoBehaviour
     public void bulletBtnClick()
     {
         // update function content
-        _fcBulletTypeText.text = "INT";
-        _fcSubTypeText.text = "Shrapnel, Tracer";
-        _fcBaseDmgText.text = "12";
-        _fcRpsText.text = "10 ";
+        _fcBulletTypeText.text = "String bullet_type = Bullet";
+        _fcSubTypeText.text = "String bullet_subType = Shrapnel, Tracer";
+        _fcBaseDmgText.text = "int base_Dmg = 12";
+        _fcRpsText.text = "int rps = 10 ";
 
-        updateWeaponList("INT", "Shrapnel", "Tracer", 8, 8, 12, 12, 10, 10, _prefabA, _prefabB);
+        _bulletText.text = "class Bullet :";
+
+        updateWeaponList("Bullet", "Shrapnel", "Tracer", 8, 8, 12, 12, 10, 10, _prefabA, _prefabB);
 
 
 
@@ -132,55 +138,53 @@ public class BuildPhase : MonoBehaviour
     public void rocketBtnClick()
     {
         // update function content
-        _fcBulletTypeText.text = "FL";
-        _fcSubTypeText.text = "Nuclear, Napalm";
-        _fcBaseDmgText.text = "60";
-        _fcRpsText.text = "1";
+        _fcBulletTypeText.text = "String bullet_type = Rocket";
+        _fcSubTypeText.text = "String bullet_subType = Nuclear, Napalm";
+        _fcBaseDmgText.text = "int base_Dmg = 60";
+        _fcRpsText.text = "int rps = 1";
 
-        updateWeaponList("FL", "Nuclear", "Napalm", 8, 8, 60, 60, 1, 1, _prefabA, _prefabB);
+        _bulletText.text = "class Rocket :";
 
-
+        updateWeaponList("Rocket", "Nuclear", "Napalm", 8, 8, 60, 60, 1, 1, _prefabA, _prefabB);
     }
 
     public void stickyBtnClick()
     {
         // update function content
-        _fcBulletTypeText.text = "STR";
-        _fcSubTypeText.text = "Plague, Singularity";
-        _fcBaseDmgText.text = "30";
-        _fcRpsText.text = "4";
+        _fcBulletTypeText.text = "String bullet_type = Sticky";
+        _fcSubTypeText.text = "String bullet_subType = Plague, Singularity";
+        _fcBaseDmgText.text = "int base_Dmg = 30";
+        _fcRpsText.text = "int rps = 4";
 
+        _bulletText.text = "class Sticky :";
 
-        updateWeaponList("STR", "Plague", "Singularity", 8, 8, 30, 30, 4, 4, _prefabA, _prefabB);
-
+        updateWeaponList("Sticky", "Plague", "Singularity", 8, 8, 30, 30, 4, 4, _prefabA, _prefabB);
     }
 
     // On Click Methods for Traits
 
     public void multiShotBtnClick()
     {
+        _selectedTrait = 0;
         // display description
         _traitDescText.text = "+ 4 Projectile count (shoots 5 projectiles with a spread at a time)\n- Range";
 
     }
 
-
     public void warpedBtnClick()
     {
+        _selectedTrait = 1;
         // display description
         _traitDescText.text = " > Bullets spawn from crosshair\n > 50 % fire rate";
 
     }
 
-
     public void rapidFireBtnClick()
     {
+        _selectedTrait = 2;
         // display description
         _traitDescText.text = "+ 30% fire rate\n- 15 % damage";
-
     }
-
-
 
     public void weap1BtnClick()
     {
@@ -196,15 +200,56 @@ public class BuildPhase : MonoBehaviour
 
     }
 
+    public void equipTraitBtnClick()
+    {
+  
+        // check currently selected trait
+        if (_selectedTrait == 0) // multishot
+        {
+            Debug.Log("Selected trait 0 ");
+            // update trait equipped text box
+            _traitEquippedText.text = "Trait equipped: Multishot";
+            // update function
+            _fcTraitText.text = "String trait = \"Multishot\"\nprint(\"+ 4 Projectile count (shoots 5 projectiles with a spread at a time), - Range\");\n\nreturn dmg;";
+        } 
+        if(_selectedTrait == 1) // warped
+        {
+            Debug.Log("Selected trait 1 ");
+            // update trait equipped text box
+            _traitEquippedText.text = "Trait equipped: Warped";
+            // update function
+            _fcTraitText.text = "String trait = \"Warped\"\nprint(\"Bullets spawn from crosshair, 50 % fire rate\");\n\nreturn dmg;";
+        }
+        
+        if(_selectedTrait == 2) // rapidfire
+        {
+            Debug.Log("Selected trait 2 ");
+            // update trait equipped text box
+            _traitEquippedText.text = "Trait equipped: Rapid-fire";
+            // update function
+            _fcTraitText.text = "String trait = \"Rapid-fire\"\nprint(\"+ 30% fire rate , -15 % damage\");\n\nreturn dmg;";
+         
+
+        }
+
+    }
+
     public void updateFunctionScreen()
     {
-        _fcBulletTypeText.text = _weapons[_selectedWeap].bullet;
-        _fcSubTypeText.text = _weapons[_selectedWeap].bulletSubTypeA;
-        _fcBaseDmgText.text = _weapons[_selectedWeap].baseDmgA.ToString();
-        _fcRpsText.text = _weapons[_selectedWeap].rpsA.ToString();
+        //
+        _bulletText.text = "class " + _weapons[_selectedWeap].bullet;
+
+        _fcBulletTypeText.text = "String bullet_type = " + _weapons[_selectedWeap].bullet;
+        _fcSubTypeText.text = "String bullet_subType = " + _weapons[_selectedWeap].bulletSubTypeA;
+        _fcBaseDmgText.text = "int base_Dmg = " + _weapons[_selectedWeap].baseDmgA.ToString();
+        _fcRpsText.text = "int rps = " + _weapons[_selectedWeap].rpsA.ToString();
+
+        // Update trait screen
+        
     }
 
 
+   
 
 
 

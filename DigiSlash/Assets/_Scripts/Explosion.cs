b8712dd767269c6888ec34c12e3bbc105faf436e
Rayden Lizan <rayden_e_lizan@dlsu.edu.ph>
2022-12-05 12:30:01 +0800

    Adjusted colors for each subtype, added dot for napalm, added explosion rotations

--- DigiSlash/Assets/_Scripts/Explosion.cs
+++ DigiSlash/Assets/_Scripts/Explosion.cs
@@ -21,7 +21,7 @@ public class Explosion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0f, 0f, Time.deltaTime * 50);
     }
 
 
